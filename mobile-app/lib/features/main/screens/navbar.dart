dd65395858342c963aea670a9e19b2622971b245
Dewa Bisma <idewagedebisma@gmail.com>
2025-07-30 17:40:00 +0800

    feat: make navbar widget UI iPad compatible

--- mobile-app/lib/features/main/screens/navbar.dart
+++ mobile-app/lib/features/main/screens/navbar.dart
@@ -6,10 +6,11 @@ import 'package:resonance_network_wallet/features/main/screens/settings_screen.d
 import 'package:resonance_network_wallet/features/main/screens/transactions_screen.dart';
 import 'package:resonance_network_wallet/features/main/screens/wallet_main.dart';
 import 'package:resonance_network_wallet/models/wallet_state_manager.dart';
+import 'package:resonance_network_wallet/shared/extensions/media_query_data_extension.dart';
 
 class NavItem {
-  final SvgPicture offIcon;
-  final SvgPicture onIcon;
+  final String offIcon;
+  final String onIcon;
   final String label;
 
   NavItem(this.offIcon, this.onIcon, this.label);
@@ -17,6 +18,12 @@ class NavItem {
 
 // Custom painter for the custom bottom navigation bar
 class BottomNavPainter extends CustomPainter {
+  final bool isTablet;
+
+  BottomNavPainter({super.repaint, required this.isTablet});
+
+  double get offset => isTablet ? 130 : 80;
+
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
@@ -29,9 +36,9 @@ class BottomNavPainter extends CustomPainter {
 
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
-    path.lineTo((size.width * 1 / 2) + 80, 0);
+    path.lineTo((size.width * 1 / 2) + offset, 0);
     path.lineTo((size.width * 1 / 2), size.height / 2);
-    path.lineTo((size.width * 1 / 2) - 80, 0);
+    path.lineTo((size.width * 1 / 2) - offset, 0);
     path.lineTo(0, 0);
 
     path.close();
@@ -95,28 +102,28 @@ class _NavbarState extends State<Navbar> {
 
   final List<NavItem> _navItems = [
     NavItem(
-      SvgPicture.asset('assets/navbar/home_icon_off.svg'),
-      SvgPicture.asset('assets/navbar/home_icon_on.svg'),
+      'assets/navbar/home_icon_off.svg',
+      'assets/navbar/home_icon_on.svg',
       'Home',
     ),
     NavItem(
-      SvgPicture.asset('assets/navbar/history_icon_off.svg'),
-      SvgPicture.asset('assets/navbar/history_icon_on.svg'),
+      'assets/navbar/history_icon_off.svg',
+      'assets/navbar/history_icon_on.svg',
       'History',
     ),
     NavItem(
-      SvgPicture.asset('assets/navbar/floating_button.svg'),
-      SvgPicture.asset('assets/navbar/floating_button.svg'),
+      'assets/navbar/floating_button.svg',
+      'assets/navbar/floating_button.svg',
       'Send',
     ),
     NavItem(
-      SvgPicture.asset('assets/navbar/settings_icon_off.svg'),
-      SvgPicture.asset('assets/navbar/settings_icon_on.svg'),
+      'assets/navbar/settings_icon_off.svg',
+      'assets/navbar/settings_icon_on.svg',
       'Settings',
     ),
     NavItem(
-      SvgPicture.asset('assets/navbar/notifications_icon_off.svg'),
-      SvgPicture.asset('assets/navbar/notifications_icon_on.svg'),
+      'assets/navbar/notifications_icon_off.svg',
+      'assets/navbar/notifications_icon_on.svg',
       'Notifications',
     ),
   ];
@@ -127,7 +134,7 @@ class _NavbarState extends State<Navbar> {
     });
   }
 
-  Widget _buildNavItem(int index, NavItem item) {
+  Widget _buildNavItem(int index, NavItem item, bool isTablet) {
     bool isSelected = index > 2
         ? _selectedIndex == index - 1
         : _selectedIndex == index;
@@ -135,13 +142,13 @@ class _NavbarState extends State<Navbar> {
     // Floating action button item
     if (index == 2) {
       return SizedBox(
-        height: 75,
-        width: 70,
+        height: isTablet ? 100 : 75,
+        width: isTablet ? 95 : 70,
         child: GestureDetector(
           onTap: () {
             Navigator.pushNamed(context, '/send');
           },
-          child: item.onIcon,
+          child: SvgPicture.asset(item.onIcon),
         ),
       );
     }
@@ -149,8 +156,8 @@ class _NavbarState extends State<Navbar> {
     // Notification item with test flag
     if (index == 4 && _notificationTestDisabled) {
       return SizedBox(
-        height: 32,
-        width: 70,
+        height: isTablet ? 40 : 32,
+        width: isTablet ? 78 : 70,
         child: InkWell(
           onTap: null,
           child: Column(
@@ -158,6 +165,7 @@ class _NavbarState extends State<Navbar> {
             children: [
               SvgPicture.asset(
                 'assets/navbar/notifications_icon_off.svg',
+                width: isTablet ? 32 : 20,
                 colorFilter: const ColorFilter.mode(
                   Colors.blueGrey,
                   BlendMode.srcIn,
@@ -174,11 +182,15 @@ class _NavbarState extends State<Navbar> {
         _onItemTapped(index);
       },
       child: SizedBox(
-        height: 32,
-        width: 70,
+        height: isTablet ? 40 : 32,
+        width: isTablet ? 78 : 70,
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [isSelected ? item.onIcon : item.offIcon],
+          children: [
+            isSelected
+                ? SvgPicture.asset(item.onIcon, width: isTablet ? 32 : 20)
+                : SvgPicture.asset(item.offIcon, width: isTablet ? 32 : 20),
+          ],
         ),
       ),
     );
@@ -198,15 +210,17 @@ class _NavbarState extends State<Navbar> {
     );
   }
 
-  Widget _buildBottomNavigationBar() {
+  Widget _buildBottomNavigationBar(bool isTablet) {
+    final height = isTablet ? 110.0 : 90.0;
+
     return Container(
       color: Colors.transparent,
-      height: 90,
+      height: height,
       child: Stack(
         children: [
           CustomPaint(
-            size: Size(MediaQuery.of(context).size.width, 90),
-            painter: BottomNavPainter(),
+            size: Size(MediaQuery.of(context).size.width, height),
+            painter: BottomNavPainter(isTablet: isTablet),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -214,7 +228,7 @@ class _NavbarState extends State<Navbar> {
               int index = entry.key;
               NavItem item = entry.value;
 
-              return _buildNavItem(index, item);
+              return _buildNavItem(index, item, isTablet);
             }).toList(),
           ),
         ],
@@ -224,10 +238,12 @@ class _NavbarState extends State<Navbar> {
 
   @override
   Widget build(BuildContext context) {
+    final isTablet = MediaQuery.of(context).isTablet;
+
     return Scaffold(
       extendBody: true,
       body: _buildBody(),
-      bottomNavigationBar: _buildBottomNavigationBar(),
+      bottomNavigationBar: _buildBottomNavigationBar(isTablet),
     );
   }
 }
