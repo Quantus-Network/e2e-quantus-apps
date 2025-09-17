e1f53124730496ddba28d2019927cc78a2b090cd
Beast <idewagedebisma@gmail.com>
2025-09-17 19:02:34 +0800

    feat: change symbol from RES to QU

--- quantus_sdk/lib/src/constants/app_constants.dart
+++ quantus_sdk/lib/src/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'Quantus Wallet';
-  static const String tokenSymbol = 'RES'; // fetch this from chain eventually
+  static const String tokenSymbol = 'QU'; // fetch this from chain eventually
   static const String shareUrl = 'https://linktr.ee/quantusnetwork';
 
   // static const String rpcEndpoint = 'ws://127.0.0.1:9944'; // local testing
