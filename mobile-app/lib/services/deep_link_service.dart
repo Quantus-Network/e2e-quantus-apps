e88980b32b76cd8488adc946e800fc8b189b4aa4
Beast <idewagedebisma@gmail.com>
2025-10-23 15:52:54 +0800

    feat: finish updating deeplink to support new format

--- mobile-app/lib/services/deep_link_service.dart
+++ mobile-app/lib/services/deep_link_service.dart
@@ -27,16 +27,23 @@ class DeepLinkService {
   }
 
   void _handleLink(Uri uri, GlobalKey<NavigatorState> navigatorKey) {
-    // Check if the path matches your expected format of /account/:id
-    if (uri.pathSegments.length == 2 && uri.pathSegments.first == 'account') {
-      final accountId = uri.pathSegments.last;
-      
-      // Use the navigator key to push the new route.
-      // We use `currentState` because the key is attached to the Navigator's state.
-      navigatorKey.currentState?.pushNamed(
-        '/account',
-        arguments: accountId, 
-      );
+    if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'account') {
+      String? accountId;
+
+      // Check for new format: /account?id=123
+      if (uri.pathSegments.length == 1 && uri.queryParameters.containsKey('id')) {
+        accountId = uri.queryParameters['id'];
+      }
+      // Check for old format: /account/123
+      else if (uri.pathSegments.length == 2) {
+        accountId = uri.pathSegments.last;
+      }
+
+      if (accountId != null && accountId.isNotEmpty) {
+        navigatorKey.currentState?.pushNamed('/account', arguments: accountId);
+      } else {
+        print('Missing or empty account id');
+      }
     }
   }
 
