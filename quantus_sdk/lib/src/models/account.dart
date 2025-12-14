aa207a8db1da3924d65058de12c1120d3445b295
Nikolaus Heger <nheger@gmail.com>
2025-12-14 16:57:40 +0800

    add account type

--- quantus_sdk/lib/src/models/account.dart
+++ quantus_sdk/lib/src/models/account.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/foundation.dart';
 
+enum AccountType {
+  local,
+  keystone,
+}
+
 @immutable
 class Account {
   final int walletIndex;
   final int index; // derivation index
   final String name;
   final String accountId; // address
-  const Account({required this.walletIndex, required this.index, required this.name, required this.accountId});
+  final AccountType accountType;
+  const Account({required this.walletIndex, required this.index, required this.name, required this.accountId, this.accountType = AccountType.local});
 
   factory Account.fromJson(Map<String, dynamic> json) {
-    return Account(walletIndex: (json['walletIndex'] ?? 0) as int, index: json['index'] as int, name: json['name'] as String, accountId: json['accountId'] as String);
+    return Account(
+      walletIndex: (json['walletIndex'] ?? 0) as int,
+      index: json['index'] as int, 
+      name: json['name'] as String, 
+      accountId: json['accountId'] as String,
+      accountType: AccountType.values.byName(json['accountType'] as String? ?? AccountType.local.name),
+    );
   }
 
   Map<String, dynamic> toJson() {
-    return {'walletIndex': walletIndex, 'index': index, 'name': name, 'accountId': accountId};
+    return {'walletIndex': walletIndex, 'index': index, 'name': name, 'accountId': accountId, 'accountType': accountType.name};
   }
 
   Account copyWith({int? walletIndex, int? index, String? name, String? accountId, int? uiPosition}) {
