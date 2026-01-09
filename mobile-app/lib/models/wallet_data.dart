e935426137eab2c6130c5d74e5e0a5b1f41bc5f6
Beast <idewagedebisma@gmail.com>
2026-01-09 12:34:59 +0800

    chore: remove unused files

--- mobile-app/lib/models/wallet_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:quantus_sdk/quantus_sdk.dart';
-
-class WalletData {
-  final Account account;
-  final BigInt balance;
-
-  WalletData({required this.account, required this.balance});
-}
--- mobile-app/lib/shared/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const String welcome = '/';
-  static const String createWallet = '/create-wallet';
-  static const String importWallet = '/import-wallet';
-  static const String walletMain = '/wallet-main';
-  static const String send = '/send';
-  static const String accountProfile = '/account-profile';
-}
