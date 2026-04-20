7b184ecb9d78dee86ba8dd645828a91cf9239c92
Beast <idewagedebisma@gmail.com>
2026-04-20 15:45:13 +0800

    feat: resolve PR review issues

--- mobile-app/lib/v2/components/loader.dart
+++ mobile-app/lib/v2/components/loader.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:resonance_network_wallet/shared/extensions/media_query_data_extension.dart';
 import 'package:resonance_network_wallet/v2/theme/app_colors.dart';
 
 class Loader extends StatelessWidget {
   final Color? color;
-  const Loader({super.key, this.color});
+  final double? size;
+
+  const Loader({super.key, this.color, this.size});
 
   @override
   Widget build(BuildContext context) {
+    final effectiveSize = size ?? (context.isTablet ? 24 : 16);
+
     return SizedBox(
-      height: 16,
-      width: 16,
+      height: effectiveSize,
+      width: effectiveSize,
       child: CircularProgressIndicator(strokeWidth: 2, color: color ?? context.colors.textSecondary),
     );
   }
